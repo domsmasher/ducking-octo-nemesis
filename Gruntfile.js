af2d300077ca7c30babf123730afa3d245b4a5cf
domsmasher <matteo.gildone@gmail.com>
2014-01-10 07:26:57 +0000

    added few form fields

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     nodeunit: {
-      files: ['test/**/*_test.js'],
+      files: ['test/**/*_test.js']
     },
     jshint: {
       options: {
@@ -19,7 +19,28 @@ module.exports = function(grunt) {
       },
       test: {
         src: ['test/**/*.js']
-      },
+      }
+    },
+    src: {
+      scripts: ['public/src/js/**/*.js'],
+      styles: ['public/src/css/**/*.css'],
+      specs: ['test/**/*.spec.js'],
+      stylus: ['stylus/*.styl']
+    },
+    stylus: {
+      compile: {
+        options: {
+          compress: false,
+          paths: ['path/to/import', 'another/to/import'],
+          linenos: false,
+          urlfunc: 'embedurl', // use embedurl('test.png') in our code to trigger Data URI embedding
+          use: [ require('nib') ],
+          import: [ 'nib' ]
+        },
+        files: {
+          'public/src/css/base.css': 'stylus/base.styl'
+        }
+      }
     },
     watch: {
       gruntfile: {
@@ -34,13 +55,22 @@ module.exports = function(grunt) {
         files: '<%= jshint.test.src %>',
         tasks: ['jshint:test', 'nodeunit']
       },
-    },
+      options: {
+        livereload: true
+      },
+      stylus: {
+        files: '<%= src.stylus %>',
+        tasks: ['stylus']
+      }
+    }
   });
 
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+
 
   // Default task.
   grunt.registerTask('default', ['jshint', 'nodeunit']);
---
+++ public/src/js/app.js
@@ -0,0 +1,11 @@
+angular.module('TPApp', [])
+    .controller('MainCtrl', function ($scope) {
+
+        $scope.header = {
+            background : '#47a3da',
+            color: '#ffffff',
+            fontSize: '16px',
+            height: '45px'
+        }
+
+    });
